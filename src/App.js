7a3f1158a1ab5dfdf944872dcdfb7d5a438457a9
unknown <zignale92@gmail.com>
2025-02-22 16:50:34 +0100

    Start

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Main from "./main";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1 / 15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
+}
+
+ 
